4eddbb3fd9e708c2fd7740f06e7337610283728e
Dmitriy Kovalev <d.g.kovalev@gmail.com>
2019-06-17 16:46:12 +0300

    about page aos animation

--- src/components/AboutMiddle.vue
+++ src/components/AboutMiddle.vue
@@ -2,18 +2,18 @@
   <div class="container about-middle-container text-center">
     <h1 class="text-uppercase h1-header mb-4">почему нам доверяют</h1>
     <div class="about-info d-flex flex-row">
-      <div class="about-info-item d-flex mt-5  flex-column align-items-center justify-content-center">
+      <div data-aos="fade-up" class="about-info-item d-flex mt-5  flex-column align-items-center justify-content-center">
         <h3 class="h3-header pt-4 pb-4">Результат</h3>
         <img src="../assets/img/handshake.svg">
         <p class="info-item-text pt-4">Вы оплачиваете только результат.
 Немалая часть заказов поступает к нам от наших постоянных клиентов. Это говорит о качестве выполняемой работы и о хорошем впечатлении, которое производят наши клинеры.</p>
       </div>
-      <div class="about-info-item d-flex mt-5 ml-5 flex-column align-items-center justify-content-center">
+      <div data-aos="fade-up" data-aos-delay="100" class="about-info-item d-flex mt-5 ml-5 flex-column align-items-center justify-content-center">
         <h3 class="h3-header pt-4 pb-4">Персонал</h3>
         <img src="../assets/img/handshake.svg">
         <p class="info-item-text pt-4">Каждый наш сотрудник — это человек, в котором мы уверены. Мы устраиваем строгий отбор и только после обучения и проверки службой безопасности допускаем клинера к работе.</p>
       </div>
-      <div class="about-info-item d-flex mt-5 ml-5 flex-column align-items-center justify-content-center">
+      <div data-aos="fade-up" data-aos-delay="200" class="about-info-item d-flex mt-5 ml-5 flex-column align-items-center justify-content-center">
         <h3 class="h3-header pt-4 pb-4">Безопасность</h3>
         <img src="../assets/img/handshake.svg">
         <p class="info-item-text pt-4">Все используемые средства являются экологически безопасными, что исключает возможность появления аллергических реакций или других неприятных последствий после уборки.</p>
