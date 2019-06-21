d0560e0eafcb054e847739e58f74b9c90c47ecc2
Dmitriy Kovalev <d.g.kovalev@gmail.com>
2019-06-21 15:24:14 +0300

    main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home-page position-relative">
-    <Banner></Banner>
-    <Middle></Middle>
+    <Banner :footer='false'>
+      <img class="home-banner-image" src="../assets/img/banner-image.svg">
+      <HomeBanner></HomeBanner>
+    </Banner>
+    <Middle>
+      <HomeMiddle></HomeMiddle>
+    </Middle>
     <Footer></Footer>
 
   </div>
@@ -9,14 +14,18 @@
 
 <script>
 import Banner from '../components/Banner.vue';
+import HomeBanner from '../components/HomeBanner.vue';
 import Middle from '../components/Middle.vue';
+import HomeMiddle from '../components/HomeMiddle.vue';
 import Footer from '../components/Footer.vue';
 
 export default {
   name: 'home',
   components: {
     Banner,
+    HomeBanner,
     Middle,
+    HomeMiddle,
     Footer,
   },
 };
@@ -25,6 +34,11 @@ export default {
 @import '../assets/scss/main.scss';
 .home-page {
 
+}
+.home-banner-image {
+  margin-top: -110px;
+  height: 100%;
+  object-fit: contain;
 }
 
 </style>
